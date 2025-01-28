de359e14a62e2c08f9178b01d8a0f0f73ce73c51
Yoshitake Hama <hama17727689@icloud.com>
2025-01-28 11:55:29 +0900

    vite.configにcssオプションの追加

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {}
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 })
